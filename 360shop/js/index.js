af4fe73c97e3af4483754f8b76084a3e0c2a2ed9
liu yun <43500876@qq.com>
2017-01-15 01:16:13 +0800

    change2

--- 360shop/js/index.js
+++ 360shop/js/index.js
@@ -1,15 +1,16 @@
 // news 轮播
     var news = $(".news-list");
     var next = 0;
-    var now = 0
-    setInterval(newMove,1500)
+    var now = 0;
+    var z=2;
+    setInterval(newMove,3500)
     function newMove(){
         next++;
         var h = news.height()
         if(next>=news.length){
             next=0;
         }
-        news.eq(next).css({marginTop:h}).animate({marginTop:0}).end().eq(now).animate({marginTop:-h});
+        news.eq(next).css({marginTop:h,zIndex:z++}).animate({marginTop:0}).end().eq(now).animate({marginTop:-h});
         now = next;
     }
 
