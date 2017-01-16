7e094fb360215980f83abd107095820f32bf17b5
[ly453890128] <43500876@qq.com>
2017-01-16 22:37:12 +0800

    change

---
+++ uek/js/index.js
@@ -0,0 +1,9 @@
+var dw = 750;
+
+function resize(){
+    var w = document.documentElement.clientWidth;
+    console.log(w)
+    document.documentElement.style.fontSize= w/dw*100+"px";
+}
+resize();
+window.onresize=resize;
