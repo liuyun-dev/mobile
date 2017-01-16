a8d1675f2018e6f1e358afdb3db2a675980b376f
[ly453890128] <43500876@qq.com>
2017-01-16 14:32:34 +0800

    day1

--- 360shop/js/index.js
+++ 360shop/js/index.js
@@ -13,5 +13,25 @@
         news.eq(next).css({marginTop:h,zIndex:z++}).animate({marginTop:0}).end().eq(now).animate({marginTop:-h});
         now = next;
     }
-
 // news end
+
+// 倒计时
+var hour = $("#hour");
+var minute = $("#minute");
+var second = $("#second");
+var end = new Date();
+end.setHours(18);
+end.setMinutes(0);
+end.setSeconds(0);
+setInterval(moveT,1000);
+function moveT(){
+    var now = new Date();
+    var time = end.getTime()-now.getTime();
+    var hours = Math.floor(time/(1000*60*60));
+    var mins = Math.floor(time/(1000*60))%60;
+    var sces=Math.round(time/1000)%60;
+    hour.html(hours<10?"0"+hours:hours)
+    minute.html(mins<10?"0"+mins:mins)
+    second[0].innerHTML=sces<10?"0"+sces:sces
+}
+moveT();
